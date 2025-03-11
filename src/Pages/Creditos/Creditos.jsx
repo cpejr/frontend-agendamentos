9ffade2df5c3287a2e2799f0ebe559330b910907
Lucas Akatsuka <ljoakatsuka@hotmail.com>
2025-03-11 04:55:25 -0300

    feat: pagina creditos

--- src/Pages/Creditos/Creditos.jsx
+++ src/Pages/Creditos/Creditos.jsx
@@ -1,9 +1,67 @@
+import { 
+    Container,
+    Content,
+    Titulos,
+    TextoTitulo,
+    Saldo,
+    TextoSaldo,
+    Extrato,
+    BotaoCredito,
+    Icone,
+    Preco,
+    ContentComprar,
+
+}from "./Styles";
+
+import { 
+    Dollar,
+    Coin,
+    Coin2,
+    MoneyBag
+
+ } from "../../Assets";
+
+ const creditos = [
+    { id: 1, valor: 50, preco: 15, icone: Dollar},
+    { id: 2, valor: 100, preco: 25, icone: Coin},
+    { id: 3, valor: 200, preco: 50, icone: Coin2},
+    { id: 4, valor: 500, preco: 100, icone: MoneyBag}
+];
+
+
 function Creditos (){
 
+    
 return(
-    <div>
-        PAGINA CREDITOS
-    </div>
+    <Container>
+        <Content>
+            <Titulos>
+                <TextoTitulo>Meus Créditos</TextoTitulo>
+            </Titulos>
+            <div style={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", gap: "12px"}}>
+                <Saldo>
+                    <p style={{color: "#063D61", fontSize: "28px"}}>Saldo Disponível</p>
+                    <div style={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "row", gap: "8px"}}>
+                        <img src={Dollar} alt="" />
+                        <TextoSaldo>9999</TextoSaldo>
+                    </div>
+                </Saldo>
+                <Extrato href="/" >Ver Extrato &gt;</Extrato>
+            </div>
+            <Titulos style={{width: "460px"}}>
+                <TextoTitulo>Comprar Créditos</TextoTitulo>
+            </Titulos>
+            <ContentComprar>
+                {creditos.map((credito) => (
+                        <BotaoCredito key={credito.id}>
+                            <img src={credito.icone} alt={`Ícone de ${credito.valor} créditos`} width="100px" height="100px" />
+                            <TextoSaldo>{credito.valor} Créditos</TextoSaldo>
+                            <Preco>R$ {credito.preco}</Preco>
+                        </BotaoCredito>
+                ))}
+            </ContentComprar>
+        </Content>
+    </Container>
 )
 }
 
--- src/Pages/Creditos/Styles.js
+++ src/Pages/Creditos/Styles.js
@@ -5,4 +5,93 @@ export const Container = styled.div`
     justify-content: center;
     align-items: center;
     height: 100%;
+`;
+
+export const Content = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-direction: column;
+    min-height: 75vh;
+`;
+
+export const Titulos = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 370px;
+    height: 90px;
+    border-radius: 21px;
+    border: solid 10px #E0EEFFDB;
+    background: rgb(167,206,253);
+    background: linear-gradient(90deg, rgba(167,206,253,1) 0%, rgba(255,245,155,1) 100%);
+`;
+
+export const TextoTitulo = styled.h1`
+    font-size: 36px;
+    color: #063D61;
+`;
+
+export const Saldo = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 370px;
+    height: 141px;
+    border-radius: 33px;
+    background-color: #FFF59BDB;
+    gap: 14px;
+`;
+
+export const TextoSaldo = styled.p`
+    color: #FFA600;
+    font-size: 28px;
+`;
+
+export const Extrato = styled.a`
+    color: #CECECE;
+    font-size: 22px;
+    cursor: pointer;
+    text-decoration: none;
+`;
+
+export const ContentComprar = styled.div`
+    display: flex; 
+    justify-content: center;
+    flex-wrap: wrap;
+    align-items: center;
+    gap: 12px;
 `
+
+export const BotaoCredito = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 10px;
+    background: #FFF59B;
+    border-radius: 21px;
+    width: 260px;
+    height: 260px;
+    text-align: center;
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);
+    cursor: pointer;
+    border: none;
+    transition: 0.3s;
+    font-weight: bold;
+
+    &:hover {
+        transform: scale(1.05);
+    }
+`;
+
+export const Icone = styled.img`
+    height: 128px;
+    width: 128px;
+`;
+
+export const Preco = styled.p`
+    font-size: 22px;
+    color: #063D61;
+`;
